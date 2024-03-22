3de8e842bbecd632f44a3bf1e4c83f46e95433b7
Pawel Krawczyk <pkrawczyk@iimcb.gov.pl>
2024-03-22 14:54:23 +0100

    nanopolish polya-pfizer subprogram

---
+++ src/nanopolish_polya_estimator_Pfizer.h
@@ -0,0 +1,14 @@
+//---------------------------------------------------------
+// Copyright 2017 Ontario Institute for Cancer Research
+// Written by Jared Simpson (jared.simpson@oicr.on.ca)
+//---------------------------------------------------------
+//
+// nanopolish_polya_estimator.cpp -- estimate the length
+// of poly-A tails for direct RNA reads
+//
+#ifndef NANOPOLISH_POLYA_PFIZER_H
+#define NANOPOLISH_POLYA_PFIZER_H
+
+int polya_Pfizer_main(int argc, char** argv);
+
+#endif
